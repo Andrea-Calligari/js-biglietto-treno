4f8c83a409d730771845b2a978286474933e5a80
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-03-04 20:43:56 +0100

    Add: fix code

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
 // Chiedere numero km e età
 const km = prompt('Inserisci il numero di km da percorrere')//string
-const years = prompt('Mettere la propria età');//string
+const years = prompt('Inserire la propria età');//string
 
 // Prezzo biglietto 
 let ticketPrice = 0.21 * km;//number
 
 
-const VarPriceUnder = ticketPrice * 20 / 100; //number
-const VarPriceOver = ticketPrice * 40 / 100;//number
+//Sconti 20% e 40% 
+const varPriceUnder = ticketPrice * 20 / 100; //number
+const varPriceOver = ticketPrice * 40 / 100;//number
 
-const TotalPrice = ticketPrice - VarPriceUnder || VarPriceOver;//number
+//Prezzi scontati 20% 40%
+const totalPriceOver = ticketPrice - varPriceOver;
+const totalPriceUnder = ticketPrice - varPriceUnder;
+
 //IF 20% sconto per minorenni
 
-
-if(years < 18){
-    console.log (' Lo sconto applicato è di €: ' + parseFloat(VarPriceUnder).toFixed(2));
-    console.log( 'il prezzo finale è di €: ' +  parseFloat(TotalPrice).toFixed(2));
+if(years <= 18){
+    console.log(' Il prezzo del biglietto è di €: ' + ticketPrice.toFixed(2));
+    console.log (' Lo sconto applicato è di €: ' +  varPriceUnder.toFixed(2));
+    console.log( 'Il prezzo scontato del biglietto è di €:' + totalPriceUnder.toFixed(2));
     
     //IF ELSE 40% sconto over 65
 
 
-}else if(years > 65){
-
-    console.log (' Lo sconto applicato è di  €: ' + parseFloat(VarPriceOver).toFixed(2));
-    console.log( 'il prezzo finale è di €: ' +  parseFloat(TotalPrice).toFixed(2));
-
-    
+}else if(years >= 65){
+    console.log(' Il prezzo del biglietto è di €: ' + ticketPrice.toFixed(2));
+    console.log (' Lo sconto applicato è di  €: ' + varPriceOver.toFixed(2));
+    console.log( 'Il prezzo scontato del biglietto è di €: ' +  totalPriceOver.toFixed(2));
 }
+
 else{
-    console.log(' il costo del biglietto è €:  ' + '€' + parseFloat(ticketPrice).toFixed(2) );
+     console.log('Il prezzo del biglietto è di €: ' + ticketPrice.toFixed(2));
 
 }
 
